09a093a7287a5965c9224848a11ea2e91d962d33
jimm_Y <124061310+i9kV@users.noreply.github.com>
2025-10-04 22:54:07 +0700

    Delete src directory

--- src/data/products.ts
+++
@@ -1,6 +0,0 @@
-import { Product } from "../types";
-
-export let products: Product[] = [
-  { id: 1, name: "เสื้อยืดที่ระลึก", price: 250 },
-  { id: 2, name: "แก้วน้ำลายไทย", price: 150 },
-];
--- src/data/users.ts
+++
@@ -1,6 +0,0 @@
-import { User } from "../types";
-
-export const users: User[] = [
-  { id: 1, username: "admin", password: "1234", role: "admin" },
-  { id: 2, username: "user1", password: "1234", role: "user" },
-];
